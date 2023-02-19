ef80c179be6900f2c04b46652f53c6297865a687
Abhishek <abhishekparmar162@gmail.com>
2023-02-19 16:17:08 +0530

    fix css

--- pages/createablog.jsx
+++ pages/createablog.jsx
@@ -92,7 +92,7 @@ const createablog = () => {
       </Head>
       <NavBar />
       <form>
-        <div className="relative z-0 w-full my-12 group ">
+        <div className="relative z-0 w-full my-28 md:my-16 group ">
           <input
             type="text"
             className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
@@ -117,12 +117,12 @@ const createablog = () => {
           value={formData.content}
           onChange={(e) => handleChange(e)}
           id="message"
-          rows="4"
+          rows="6"
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Write something in detail..."
         ></textarea>
 
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        <label className="block mb-2 text-sm my-4 font-medium text-gray-900 dark:text-white">
           Upload file
         </label>
         <input
@@ -152,7 +152,7 @@ const createablog = () => {
         <button
           onClick={PostBlog}
           type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white my-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Post
         </button>
