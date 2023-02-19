c8db9e893241687c40cc26c0cf591814c371e54a
Abhishek <abhishekparmar162@gmail.com>
2023-02-19 15:48:09 +0530

    finished project

--- next.config.js
+++ next.config.js
@@ -7,4 +7,14 @@ module.exports = {
   images: {
     domains: ['assets.manutd.com','firebasestorage.googleapis.com'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/blog',
+        permanent: true,
+      },
+    ]
+  },
+
 }
